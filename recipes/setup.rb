a8b8e8d2c4f3edacd93742b568146f91e4e7bd79
abhisriamity6 <root@mysql1.nwtraders.com>
2022-03-01 18:10:27 +0530

    Added variable in template

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -5,8 +5,10 @@ package 'git' do
     action :install
 end
 template '/etc/motd' do
-        
 	source 'motd.erb'
+	variables(
+		:email => 'abhi_sri_amiy6@yahoo.com'
+)
 	owner 'root'
 	group 'root'
 end
